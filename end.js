558042ce54c50c5b10f487784a9fd9ba798d358f
zainabmeerza <zainabmeerza@gmail.com>
2021-09-11 01:44:00 +0000

    fix save score button

--- end.js
+++ end.js
@@ -1,13 +1,3 @@
-/*jshint esversion: 6 */
-
-// Blinking Game Over heading - https://www.youtube.com/watch?v=9iDm3QiIEzU
-setInterval(function () {
-    let gameOverText = document.getElementById('game-over-heading');
-
-    gameOverText.innerHTML = "Game Over!";
-
-    gameOverText.style.display = (gameOverText.style.display == 'none' ? '' : 'none');
-}, 600);
 
 // Set variables 
 const username = document.getElementById('username');
@@ -18,10 +8,7 @@ const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
 // Max number of scores that can be displayed
 const maxHighScores = 5;
-
-// Set the final score that is displayed
-finalScore.innerText = mostRecentScore;
-
+ 
 // Disable the save score button if there is no value in the username
 username.addEventListener('keyup', () => {
     saveScore.disabled = !username.value;
@@ -46,7 +33,8 @@ document.getElementById("save-score-form").addEventListener('submit', function(e
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
    /* window.location.assign('https://8000-yellow-weasel-tiu1kpfg.ws-eu16.gitpod.io/end.html');*/
-   /* window.location.assign('#')*/
+   document.getElementById('quizContainer').style.display = "none";
+    window.location.assign('#');
     
 });
 
